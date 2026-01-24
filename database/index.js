6326e75d11bec77c669a8e94c5974639ed298185
bennyasi <bennyasi64@gmail.com>
2026-01-24 23:23:56 +0100

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,1 +1,22 @@
+const { Pool } = require("pg");
+require("dotenv").config();
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" 
+    ? { rejectUnauthorized: false } 
+    : false,
+});
+
+module.exports = {
+  query: async (text, params) => {
+    try {
+      const res = await pool.query(text, params);
+      console.log("executed query", { text });
+      return res;
+    } catch (error) {
+      console.error("error in query", { text, error: error.message });
+      throw error;
+    }
+  },
+};
